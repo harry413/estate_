920db72d7a07d5ca2a77f77ede78172d2b904f14
sachin413 <sachinpatidar413@gmail.com>
2023-12-15 17:33:10 +0530

    tailwindcss and basic setup

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+ 
+
+  return (
+    <>
+      <div className='text-gray-500 text-5xl flex items-center justify-center underline'>Estate World</div>
+    </>
+  )
+}
+
+export default App
