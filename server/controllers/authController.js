aaea5e13bbc20827a8d2d3cb9fc4dd607902651e
sachin413 <sachinpatidar413@gmail.com>
2023-12-25 16:28:05 +0530

    signin api route

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,6 +1,10 @@
+import dotenv from "dotenv"
+dotenv.config();
+
 import UserModel from "../models/User_Model.js";
-import bcryptjs from "bcryptjs"
+import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import jwt from 'jsonwebtoken';
 
 
 
@@ -15,4 +19,26 @@ export const signup = async(req, res, next) => {
     }catch(error){
         next(error);   
     }
+};
+export const signin = async(req, res, next) => {
+    const {email, password} = req.body;
+    
+try{
+    const existingUser = await UserModel.findOne({email});
+    if(!existingUser){
+        return next(errorHandler(404, 'User not found!'));
+    }
+    const validPassword = bcryptjs.compareSync(password, existingUser.password);
+    if(!validPassword){
+        return next(errorHandler(401, 'Invalid password'));
+    }
+    const token = jwt.sign({ id: existingUser._id }, process.env.JWT_SECRET);
+    const { password:pass, ...rest } = existingUser._doc;
+    res
+        .cookie('access_token', token, {httpOnly: true})
+        .status(200)
+        .json(rest);
+}catch(error){
+    next(error);   
+}
 };
