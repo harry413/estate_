39b43ec8d84e2e9e1aa27c8e36c89f39a64ff228
sachin413 <sachinpatidar413@gmail.com>
2023-12-29 14:29:29 +0530

    delete user functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -2,7 +2,10 @@ import { useState, useRef,useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 import {getDownloadURL, getStorage, ref, uploadBytesResumable} from  'firebase/storage'
 import { app } from '../firebase.js'
-import { updateUserStart, updateUserSuccess, updateUserFailure } from '../redux/user/userSlice.js';
+import { 
+        updateUserStart, updateUserSuccess, updateUserFailure, 
+        deleteUserFailure, deleteUserStart, deleteUserSuccess 
+      } from '../redux/user/userSlice.js';
 
 
 const Profile = () => {
@@ -67,6 +70,25 @@ const Profile = () => {
       dispatch(updateUserFailure(error.message));
     }
    }
+   const handleDeleteUser = async() => {
+    try{
+      dispatch(deleteUserStart());
+      const res = await fetch(`/api/user/delete/${currentUser._id}`, {
+        method: 'DELETE',
+      });
+      const data = await res.json();
+      if(data.success === false) {
+        dispatch(deleteUserFailure(data.message));
+        return;
+      }
+      dispatch(deleteUserSuccess(data));
+    }catch(error){
+      dispatch( deleteUserFailure(error.message) )
+    }
+   }
+
+
+
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
@@ -75,7 +97,7 @@ const Profile = () => {
         <img onClick={() => fileRef.current.click()} src={formData.avatar || currentUser.avatar} alt='photo' className='rounded-full object-cover cursor-pointer self-center mt-2 h-24 w-24'/>
         <p className=' flex self-align items-center justify-center'>
           {
-          fileError  
+          fileError 
                     ? (<span className='text-red-700'>Enable to upload image(image should be less then 2MB)</span>) 
                     : (imagePerc > 0 && imagePerc < 100 ? (
                       <span className='text-slate-700'>{`uploading ${imagePerc}%`}</span>) : 
@@ -114,7 +136,7 @@ const Profile = () => {
           </button>
       </form>
       <div className='flex justify-between mt-5'>
-        <span className='text-red-700 cursor-pointer'>Delete account</span>
+        <span className='text-red-700 cursor-pointer' onClick={handleDeleteUser}>Delete account</span>
         <span className='text-red-700 cursor-pointer'>Sign out</span>
       </div>
       <p className='text-red-700 mt-4'>{error ? error : ''}</p>
--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -31,4 +31,15 @@ export const updateUser = async(req,res,next) => {
     } catch (error) {
         next(error)
     }
+};
+
+export const deleteUser = async(req, res, next) => {
+    if(req.user.id !== req.params.id) return next(errorHandler(401, "you cannot delete this account!"))
+    try {
+        await UserModel.findByIdAndDelete(req.params.id);
+        res.clearCookies('access_token');
+        res.status(200).json('User has been deleted...')
+    } catch (error) {
+        next(error)
+    }
 }
