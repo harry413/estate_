96c92f51c8d6baf46d86513528ee5e30623062a7
sachin413 <sachinpatidar413@gmail.com>
2024-01-04 19:22:51 +0530

    adding functionality to listing page

--- server/controllers/listingController.js
+++ server/controllers/listingController.js
@@ -3,7 +3,7 @@ import Listing from '../models/ListingModel.js'
 
 export const createListing = async(req, res, next) => {
     try {
-        const listing = await Listing.Create(req.body);
+        const listing = await Listing.create(req.body);
         
         return res.status(201).json(listing);
     } catch (error) {
--- server/models/ListingModel.js
+++ server/models/ListingModel.js
@@ -50,7 +50,7 @@ const ListingSchema = new mongoose.Schema(
             type: Array,
             required: true,
         },
-        useRef: {
+        userRef: {
             type: String,
             required: true,
         },
