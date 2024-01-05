6121e485ab69e50d47820e9c120f93b655d3fff6
sachin413 <sachinpatidar413@gmail.com>
2024-01-05 13:31:45 +0530

    update userlisting route

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -120,7 +120,21 @@ const Profile = () => {
     }
    }
 
-
+   const handleDeleteListing = async(listingId) => {
+    try {
+      const res = await fetch(`/api/listing/delete/${listingId}`, {
+        method: 'DELETE',
+      })
+      const data = await res.json();
+      if(data.success === false){
+        console.log(data.message);
+        return;
+      }
+      setUserListing((prev) => prev.filter((listing) => listing._id !== listingId))
+    } catch (error) {
+      console.log(error.message);
+    }
+   }
 
   return (
     <div className='p-3 max-w-lg mx-auto'>
@@ -190,7 +204,7 @@ const Profile = () => {
                 <p className='text-slate-600 font-semibold  hover:underline truncate'>{listing.name}</p>
               </Link>
               <div className="flex flex-col items-center ">
-                <button className='text-red-700 uppercase'>Delete</button>
+                <button onClick={() => handleDeleteListing(listing._id)} className='text-red-700 uppercase'>Delete</button>
                 <button className='text-green-700 uppercase'>Edit</button>
               </div>
           </div>
--- server/controllers/listingController.js
+++ server/controllers/listingController.js
@@ -1,4 +1,5 @@
 import Listing from '../models/ListingModel.js'
+import { errorHandler } from "../utils/error.js";
 
 
 export const createListing = async(req, res, next) => {
@@ -10,3 +11,37 @@ export const createListing = async(req, res, next) => {
         next(error);
     }
 };
+
+export const deleteListing = async(req, res, next) =>{
+    const listing = await Listing.findById(req.params.id);
+
+    if(!listing){
+        return next(errorHandler(404, 'listing not found!'));
+    }
+    if(req.user.id !== listing.userRef){
+        return next(errorhandler(401, 'you cannot delete this listing'));
+    }
+    try{
+        await Listing.findByIdAndDelete(req.params.id);
+        res.status(200).json('listing delete successfully');
+    }catch(error){
+        next(error);
+    }
+}
+
+export const updateListing = async(req, res, next) => {
+    const listing = await Listing.findById(req.params.id);
+
+    if(!listing){
+        return next(errorHandler(404, 'listing not found!'));
+    }
+    if(req.user.id !== listing.userRef){
+        return next(errorhandler(401, 'you cannot update this listing'));
+    }
+    try {
+        const updateListing = await Listing.findByIdAndUpdate(req.params.id, req.body, {new: true});
+        res.status(200).json(updateListing)
+    } catch (error) {
+        next(error)
+    }
+}
