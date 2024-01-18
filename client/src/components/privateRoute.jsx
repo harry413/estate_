948d25c7f084e33385579bb4f92f15fa480fdfa2
sachin413 <sachinpatidar413@gmail.com>
2024-01-18 09:40:38 +0530

    fix privateRoute

--- client/src/components/privateRoute.jsx
+++ client/src/components/privateRoute.jsx
@@ -3,10 +3,10 @@ import { useSelector } from 'react-redux'
 import { Navigate,Outlet } from 'react-router-dom';
 
 
-const PrivateRoute = () => {
+export const PrivateRoute = () => {
     const {currentUser} = useSelector((state) => state.user);
 
   return  currentUser ? <Outlet/> : <Navigate to='/sign-in'/>;
 }
 
-export default PrivateRoute;
+
