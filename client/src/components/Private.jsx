c13b54265defa120bc3a4ed5a7027cb82e4780be
sachin413 <sachinpatidar413@gmail.com>
2024-01-18 14:35:28 +0530

    fix bug

--- client/src/components/Private.jsx
+++ client/src/components/Private.jsx
@@ -3,7 +3,7 @@ import { useSelector } from 'react-redux'
 import { Navigate,Outlet } from 'react-router-dom';
 
 
-export default PrivateRoute = () => {
+export default Private = () => {
     const {currentUser} = useSelector((state) => state.user);
 
   return  currentUser ? <Outlet/> : <Navigate to='/sign-in'/>;
